e2b227de948802955b522d63d9917afaf8978a05
hshen12 <hshen12@dons.usfca.edu>
2017-09-02 18:00:22 -0700

    Add files via upload
    please review @toropeza @shayromayo

---
+++ FibonacciRecursive.java
@@ -0,0 +1,18 @@
+package edu.usfca.cs.cs245;
+
+public class FibonacciRecursive implements Fibonacci{
+	
+
+	@Override
+	public int fibonacci(int n) {
+		
+		if(n == 0) {
+			return 0;
+		} else if(n == 1) {
+			return 1;
+		}
+		return fibonacci(n-1) + fibonacci(n -2);
+
+	}
+
+}
